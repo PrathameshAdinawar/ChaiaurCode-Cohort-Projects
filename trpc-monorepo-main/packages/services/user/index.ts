96d380bcf402a4f99c7576e6fbb157662c30a2b8
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2026-05-19 22:52:26 +0530

    drizzle, db setup and validation

--- trpc-monorepo-main/packages/services/user/index.ts
+++ trpc-monorepo-main/packages/services/user/index.ts
@@ -1,29 +1,30 @@
-import { db } from "@repo/database";
-import { usersTable } from "@repo/database/schema";
-import { env } from "../env";
-import { googleOAuth2Client } from "../clients/google-oauth";
-import { GetAuthenticationMethodOutputSchema } from "./model";
 
-class UserService {
-  public async getAuthenticationMethods(): Promise<
-    ReadonlyArray<GetAuthenticationMethodOutputSchema>
-  > {
-    const supportedAuthenticationProviders: GetAuthenticationMethodOutputSchema[] = [];
+import {randomBytes,createHmac} from 'node:crypto'
+import {db,eq} from '@repo/database'
+import {usersTable} from '@repo/database/models/user'
+import {createUserWithEmailAndPasswordInput, type CreateUserWithEmailAndPasswordInput} from './model'
 
-    const isGoogleConfigured = !!(env.GOOGLE_OAUTH_CLIENT_ID && env.GOOGLE_OAUTH_CLIENT_SECRET);
+class UserService{
 
-    if (isGoogleConfigured) {
-      const url = googleOAuth2Client.generateAuthUrl();
-      supportedAuthenticationProviders.push({
-        provider: "GOOGLE_OAUTH",
-        displayName: "Google",
-        displayText: "Signin with Google",
-        authUrl: url,
-      });
+    private async getUserByEmail(email:string){
+        const result = await db.select().from(usersTable).where(eq(usersTable.email,email))
+
+        if(!result || result.length === 0) return null
+        return result[0] 
     }
 
-    return supportedAuthenticationProviders;
-  }
+    public async createUserWithEmailAndPassword(payload: CreateUserWithEmailAndPasswordInput){
+        const {fullName,email,password} = await createUserWithEmailAndPasswordInput.parseAsync(payload)
+
+        //check if user already exists or not
+        const existingUserWithEmail = await this.getUserByEmail(email)
+        if(existingUserWithEmail) throw new Error(`user with email ${email} already exists`)
+
+        const salt = randomBytes(16).toString('hex')
+        const hash = createHmac('sha256',salt).update(password).digest('hex')
+
+        await db.insert(usersTable).values({email,fullName})
+    }
 }
 
-export default UserService;
+export default UserService
